c4b9a305aee64642c2fe41a663b58605f1f09050
Fawzaan <fawzi20112001@gmail.com>
2021-04-02 17:22:25 +0530

    binary-search-tree.cpp added

---
+++ binary-search-tree.cpp
@@ -0,0 +1,57 @@
+// 02/04/2021
+
+# include <iostream>
+
+using namespace std;
+
+class Node {
+    public:
+    int data;
+    Node *lc, *rc;
+};
+
+class BSTree {
+    public:
+    Node *root;
+
+    BSTree() {
+        root = NULL;
+    }
+
+    void create(int);   // Takes data as parameter
+    void insertNode(Node *, Node *);    // One for previous node, one for new node
+    void inorderTraversal(Node *);
+    void display();
+};
+
+void BSTree :: create(int data) {
+    Node *new_node = new Node;
+    new_node -> data = data;
+    new_node -> lc = NULL;
+    new_node -> rc = NULL;
+
+    if (root == NULL) {
+        root = new_node;
+    }
+    else {
+        insertNode(root, new_node);
+    }
+}
+
+void BSTree :: display() {
+    Node *temp;
+    temp = root;
+    inorderTraversal(temp);
+}
+
+void BSTree :: inorderTraversal(Node *t) {
+    inorderTraversal(t -> lc);
+    cout << t -> data;
+    inorderTraversal(t -> rc);
+}
+
+int main() {
+
+
+    return 0;
+}
