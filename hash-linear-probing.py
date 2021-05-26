8db5b945e1011c8c2e26bccf9d8c24f17513187c
Fawzaan <fawzi20112001@gmail.com>
2021-05-26 18:16:58 +0530

    Added hash-linear-probing.py

---
+++ hash-linear-probing.py
@@ -0,0 +1,79 @@
+# To write the insertIntoHashIndexWithReplacement() function properly
+
+from collections import defaultdict
+
+count = 0
+
+class HashLinearProbing:
+    def __init__(self, size):
+        self.hash_table = defaultdict().fromkeys(range(size), -1)
+        print(self.hash_table)
+    
+    def hashing(self, key):
+        return(key % size)
+
+    def insertIntoHashIndexWithoutReplacement(self, key, index):
+        global count
+
+        if self.hash_table[index] == -1:
+            self.hash_table[index] = key
+            count = count + 1
+        else:
+            self.findVacantIndex(key, index)
+
+    def insertIntoHashIndexWithReplacement(self, key, index):
+        global count
+
+        if self.hash_table[index] == -1:
+            self.hash_table[index] = key
+            count = count + 1
+        
+    def findVacantIndex(self, key, index):
+        for i in range(1, size):
+            pos = (index + 1) % size
+            if self.hash_table[pos] == -1:
+                self.hash_table[pos] = key
+                break
+
+    def display(self):
+        print("Hash Table Keys")
+        print(self.hash_table)
+
+        for itr in self.hash_table.items():
+            print(itr)
+
+# ------------------------------------------------------ #
+
+print("In main\n")
+size = int(input("Enter the size of the table: "))
+hash_object = HashLinearProbing(size)
+
+while 1:
+    print("Enter 1 for without replacement, 2 for with replacement, 3 for search a key, 4 for display the table and 5 to exit: ")
+    user_choice = int(input("Enter the option: "))
+
+    if user_choice == 1:    
+        key_value = int(input("Enter key value: "))
+        index = hash_object.hashing(key_value)
+        hash_object.insertIntoHashIndexWithoutReplacement(key_value, index)
+        hash_object.display()
+    elif user_choice == 2:
+        key_value = int(input("Enter key value: "))
+        index = hash_object.hashing(key_value)
+        hash_object.insertIntoHashIndexWithReplacement(key_value, index)
+        hash_object.display()
+    elif user_choice == 3:
+        key_value = int(input("Enter key value: "))
+        # Search function
+    elif user_choice == 4:
+        hash_object.display()
+    elif user_choice == 5:
+        break
+    else:
+        print("Please enter a valid input.")
+
+print("Thank you for using the Hash Linear Probing program.")
+
+
+
+
