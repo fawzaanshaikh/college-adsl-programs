83c3a120d1a368260a5c1b7269536a1cc4596d51
Fawzaan <fawzi20112001@gmail.com>
2021-06-04 22:58:12 +0530

    Delete word in dictionary functionality added

--- self-dictionary-bst.cpp
+++ self-dictionary-bst.cpp
@@ -30,13 +30,15 @@ class Dictionary {
     Node* searchInDictionary();
     void displayInorder(Node*);
     void updateInDictionary();
+    Node* minValueNode(Node*);
+    Node* deleteNode(Node*, char[]);
     void deleteInDictionary();
 };
 
 /* Definitions of member functions of Dictionary class */
 // createNode function - creates a new node of the tree
 Node* Dictionary :: createNode(char w[], char m[]) {
-    Node* new_node = new Node;
+    struct Node* new_node = new Node;
 
     for (int i = 0; i < MAX_SIZE; i++) {    // Assigning the char arrays
         new_node -> word[i] = w[i];
@@ -49,17 +51,17 @@ Node* Dictionary :: createNode(char w[], char m[]) {
 
 // insertIntoDictionary function - inserts a new node into the existing tree
 void Dictionary :: insertIntoDictionary() {
-    char wd[20], mn[20];
+    char insert_word[MAX_SIZE], insert_meaning[MAX_SIZE];
     cout << "\nEnter the word (max. letter size = 20): ";
-    cin >> wd;
+    cin >> insert_word;
     cout << "Enter the meaning of the word (max. letter size = 20): ";
-    cin >> mn;
+    cin >> insert_meaning;
 
     if (root == NULL)
-        root = createNode(wd, mn);
+        root = createNode(insert_word, insert_meaning);
     else {
-        Node* new_node = createNode(wd, mn);
-        Node* temp = root;
+        struct Node* new_node = createNode(insert_word, insert_meaning);
+        struct Node* temp = root;
 
         while (true) {
             if (strcmp(new_node -> word, temp -> word) < 0) {
@@ -88,22 +90,22 @@ Node* Dictionary :: searchInDictionary() {
     if (root == NULL) 
         cout << "\nThe dictionary is empty.\n";
     else {
-        char wd[20];
+        char search_word[MAX_SIZE];
         cout << "\nEnter the word you would like to search: ";
-        cin >> wd;
+        cin >> search_word;
 
-        Node* temp = root;
+        struct Node* temp = root;
         while (true) {
             if (temp == NULL) {
-                cout << "Could not find the word " << wd << ".\n";
+                cout << "Could not find the word " << search_word << ".\n";
                 return NULL;
             }
-            else if (strcmp(wd, temp -> word) < 0)
+            else if (strcmp(search_word, temp -> word) < 0)
                 temp = temp -> left;
-            else if (strcmp(wd, temp -> word) > 0)
+            else if (strcmp(search_word, temp -> word) > 0)
                 temp = temp -> right;
             else {
-                cout << "Found " << wd << "! It's meaning is " << temp -> meaning << ".\n";
+                cout << "Found " << search_word << "! It's meaning is " << temp -> meaning << ".\n";
                 return temp;
             }
         }
@@ -123,7 +125,7 @@ void Dictionary :: displayInorder(Node* node) {
 
 // updateInDictionary - finds the word specified by the user and updates its meaning
 void Dictionary :: updateInDictionary() {
-    Node* node = searchInDictionary();  // Searches a word and returns its node if it exists
+    struct Node* node = searchInDictionary();  // Searches a word and returns its node if it exists
     
     if (node != NULL) {
         cout << "\nEnter the new meaning of the word " << node -> word << ": ";
@@ -135,9 +137,78 @@ void Dictionary :: updateInDictionary() {
         cout << "\nThe word cannot be updated.\n";
 }
 
+// minValueNode - searches the node with the minimum most value from the right subtree of the selected node
+Node* Dictionary :: minValueNode(Node* node)
+{
+    struct Node* current = node;
+ 
+    /* loop down to find the leftmost leaf */
+    while (current && current->left != NULL)
+        current = current->left;
+ 
+    return current;
+}
+
+// deleteNode - deletes the node the same way it is done in a BST
+Node* Dictionary :: deleteNode(Node* node, char delete_word[]) {
+    if (strcmp(delete_word, node -> word) < 0)
+        root -> left = deleteNode(root -> left, delete_word);   // Checking the side where the word comes in the tree
+    else if (strcmp(delete_word, node -> word) > 0)
+        root -> right = deleteNode(node -> right, delete_word);
+    
+    else if (strcmp(delete_word, node -> word) == 0) {           // If the word is found    
+        // If the node has no children
+        if (node -> left == NULL && node -> right == NULL) {
+            free(node);
+            return NULL;
+        }
+        // If the node has one child
+        else if (node -> left == NULL) {
+            struct Node* temp = node -> right;
+            free(node);
+            return temp; 
+        }
+        else if (node -> right == NULL) {
+            struct Node* temp = node -> left;
+            free(node);
+            return temp;
+        }
+        // If the node has both children, then the inorder successor replaces the deleted node
+        else {
+            struct Node* temp = minValueNode(node -> right);    // root -> right to find the smallest value in the right subtree
+            
+            for (int i = 0; i < MAX_SIZE; i++) {    // Assigning the char arrays
+                node -> word[i] = temp -> word[i];
+                node -> meaning[i] = temp -> meaning[i];
+            }
+
+            root -> right = deleteNode(node -> right, delete_word); // Delete the inorder successor
+        }
+
+        return root;
+    }
+
+    else
+        return NULL;
+}
+
+ 
 // deleteInDictionary - finds the word and deletes it and its meaning
 void Dictionary :: deleteInDictionary() {
-    Node* node = searchInDictionary();  // Searches a word and returns its node if it exists
+    if (root == NULL)
+        cout << "\nThe dictionary is empty.\n";
+    else {
+        struct Node* temp = root;
+        char delete_word[MAX_SIZE];
+        cout << "\nEnter the word you want to delete: ";
+        cin >> delete_word;
+
+        struct Node* check = deleteNode(root, delete_word);
+        if (check != NULL)
+            cout << "\nThe word was deleted.\n" << check -> word ;
+        else
+            cout << "\nThe word could not be found, hence not deleting\n" << check-> word;
+    }
 
 }
 
@@ -148,10 +219,10 @@ int main() {
     cout << "\n\n------ Dictionary Application ------";
     
     while (true) {
-        cout << "\nEnter 1 to enter a word and its meaning, 2 to display the dictionary, 3 to search a word, 4 to update a word, 7 to exit: ";
+        cout << "\n  Enter 1 to enter a word and its meaning\n\t2 to display the dictionary\n\t3 to search a word\n\t4 to update a word\n\t5 to delete a word\n\t6 to exit: ";
         cin >> user_input;
 
-        if (user_input == 7)
+        if (user_input == 6)
             break;
 
         switch(user_input) {
@@ -168,6 +239,9 @@ int main() {
                 break;
             case 4:
                 dObj.updateInDictionary();
+                break;
+            case 5:
+                dObj.deleteInDictionary();
                 break;
             default:
                 cout << "\nInvalid Entry";
